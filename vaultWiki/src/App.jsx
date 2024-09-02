1a8de08df40ba3d29e93085991aa2889856c44d8
Leticia-SS <leticia.saraiva013@gmail.com>
2024-09-02 08:55:21 -0300

    update app.jsx

--- vaultWiki/src/App.jsx
+++ vaultWiki/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -7,9 +8,17 @@ function App() {
 
   return (
     <>
-      <div>
+     
+    </>
+  )
+}
+
+export default App
+
+/*
+ <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={reactLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -27,8 +36,4 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
-}
-
-export default App
+*/
