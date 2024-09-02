e24e0dce66d6fb50341d0cf095a25db873a30f13
Leticia-SS <leticia.saraiva013@gmail.com>
2024-09-02 09:13:01 -0300

    edit app.jsx

--- vaultWiki/src/App.jsx
+++ vaultWiki/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+ // const characters = 
 
   return (
     <>
-     
+     <h1>In Progress</h1>
     </>
   )
 }
@@ -16,6 +17,9 @@ function App() {
 export default App
 
 /*
+
+const [count, setCount] = useState(0)
+
  <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
